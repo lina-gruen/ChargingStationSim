50cb669db5288535eebd20ec971b837c5dd153a9
lina-gruen <lina.grunbeck@gmail.com>
2023-01-18 16:24:13 +0100

    Add documentation to Vehicle class and change input to dict parameters.

--- chargingStationSim/vehicle.py
+++ chargingStationSim/vehicle.py
@@ -8,37 +8,54 @@ __author__ = 'Lina Grünbeck / lina.grunbeck@gmail.com'
 
 class Vehicle:
     """
-    Base class for the all vehicles in a vehicle fleet.
+    Base class for all vehicles in a vehicle fleet.
     """
-    #Default efficiency measured in km/kWh.
-    default_eff = 5
 
-    def __init__(self, weight_class, dist_type, capacity, adapter, max_pow, drive_dist, soc):
-        """
+    default_params = {'weight_class': None, 'dist_type': None, 'capacity': None, 'efficiency': None,
+                      'drive_dist': None, 'soc': None}
 
+    def __init__(self, params):
         """
-        self.weight_class = weight_class
-        self.dist_type = dist_type
-        self.capacity = capacity
-        self.adapter = adapter
-        self.max_pow = max_pow
-        self.drive_dist = drive_dist
-        self.soc = soc
+        Parameters
+        ----------
+        params: dict
+            New parameters for a battery.
+            Including:
+                weight_class: string
+                    If vehicle is medium-heavy or heavy
+                dist_type: string
+                    If the vehicle is for short-distance (local) or
+                    long-distance travel.
+                capacity: int
+                    Max kWh rating of the vehicle battery.
+                efficiency: int
+                    Efficiency of the battery measured in km/kWh.
+                drive_dist: int
+                    Length of the next trip planned in km.
+                soc: int
+                    State of Charge of the vehicle battery.
+        """
+        self.weight_class = params['weight_class']
+        self.dist_type = params['dist_type']
+        self.capacity = params['capacity']
+        self.efficiency = params['efficiency']
+        self.drive_dist = params['drive_dist']
+        self.soc = params['soc']
         self.demand = None
         self.wait_time = None
+        # self.max_pow = params['max_pow']
 
     def new_demand(self, new_dist, rest):
         """
-
-        :return:
+        Calculate the new demand of kWh the vehicle has for its next trip.
         """
         if rest:
             self.wait_time = 90
         else:
-            self.demand = self.default_eff/new_dist
+            self.demand = self.default_eff / new_dist
 
     def charge_vehicle(self, power):
         """
-
-        :return:
+        Charge the vehicle by a certain amount.
         """
+        pass
