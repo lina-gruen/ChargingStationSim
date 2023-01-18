9d7a1f14f15a95585fcd9717dfc093dda2e38ae1
lina-gruen <lina.grunbeck@gmail.com>
2023-01-18 16:22:07 +0100

    Add Charger class.

--- chargingStationSim/charger.py
+++ chargingStationSim/charger.py
@@ -4,3 +4,15 @@ This file contains the Charger class
 """
 
 __author__ = 'Lina Grünbeck / lina.grunbeck@gmail.com'
+
+
+class Charger:
+    """
+    Class for a charger.
+    """
+
+    def __init__(self):
+        """
+
+        """
+        self.occupied = False
