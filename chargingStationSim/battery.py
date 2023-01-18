e8a90fbeddceb135010cf529d41a7097878e6475
lina-gruen <lina.grunbeck@gmail.com>
2023-01-18 14:59:27 +0100

    Add initial params and first methods to Battery class.

--- chargingStationSim/battery.py
+++ chargingStationSim/battery.py
@@ -4,3 +4,51 @@ This file contains the Battery class
 """
 
 __author__ = 'Lina Grünbeck / lina.grunbeck@gmail.com'
+
+
+class Battery:
+    """
+    Class for a local battery pack at a station.
+    """
+
+    def __init__(self, capacity, max_pow, soc):
+        """
+
+        """
+        self.capacity = capacity
+        self.max_pow = max_pow
+        self.soc = soc
+
+    def recharge(self, available):
+        """
+        Recharge the battery a certain amount.
+
+        Parameters
+        ----------
+        available: int
+            Amount of kWh given to the battery.
+        """
+        if available < 0:
+            raise ValueError('Battery was given negative amount of kWh.')
+        new_soc = self.soc + (available / self.capacity)
+        if new_soc > 100:
+            self.soc = 100
+        else:
+            self.soc = new_soc
+
+    def discharge(self, demand):
+        """
+        Discharge the battery a certain amount.
+
+        Parameters
+        ----------
+        demand: int
+            Amount of kWh needed from the battery.
+        """
+        if demand < 0:
+            raise ValueError('Demanded negative amount of kWh from battery.')
+        new_soc = self.soc - (demand / self.capacity)
+        if new_soc < 0:
+            self.soc = 0
+        else:
+            self.soc = new_soc
