a7501e19e3a3f7f399d5ae36b96b2cb9540ba49a
lina-gruen <lina.grunbeck@gmail.com>
2023-01-31 13:14:16 +0100

    Add power capability and possibility to save charger, remove new_route_len() and add check_power()

--- chargingStationSim/vehicle.py
+++ chargingStationSim/vehicle.py
@@ -23,7 +23,7 @@ class Vehicle(Agent):
         Parameters
         ----------
         vehicle_id: int
-            Unique id for the battery.
+            Unique id for the vehicle.
         station: mesa.model
             Instance of the station that contains the vehicle.
         params: dict
@@ -45,18 +45,26 @@ class Vehicle(Agent):
         """
         self.id = vehicle_id
         self.station = station
+        self.time = station.time_step  # time per iteration step in minutes
         self.weight_class = params['weight_class']
         self.dist_type = params['dist_type']
         self.capacity = params['capacity']
         self.drive_dist = params['drive_dist']
-        self.soc = soc
         if params['efficiency'] is None:
             self.efficiency = 5
         else:
             self.efficiency = params['efficiency']
+        self.soc = soc
+        # kWh needed for the vehicle.
         self.demand = 0
+        # number of iterations the vehicle will be driving.
         self.driving = 0
-        self.charge = False
+        # The power the vehicle is currently charging with.
+        self.power = 0
+        # The charger the vehicle is using. None if not charging.
+        self.charger = None
+        # If the vehicle is charging or not.
+        self.charging = False
         # self.wait_time = None
         # self.max_pow = params['max_pow']
 
@@ -66,48 +74,50 @@ class Vehicle(Agent):
         """
         return self.soc
 
-    @staticmethod
-    def new_route_len():
+    def new_demand(self):
         """
-        Find the new driving distance for the vehicles next trip after charging.
-
-        Returns
-        -------
-        route_len: int
-            new driving distance.
+        Calculate the new demand of kWh the vehicle needs for its next
+        trip, depending on the length of the new route.
         """
         route_len = 75
-        return route_len
-
-    def new_demand(self, route_len):
-        """
-        Calculate the new demand of kWh the vehicle needs for its next trip.
-        """
         self.demand = route_len / self.efficiency
         # target_soc enten 100 eller utifra demand, utifra om det er lokal eller langdistanse.
+
+    def check_power(self):
+        """
+        Check is the power output of the charger has changed since the last step.
+        """
+        if self.power == self.charger.power:
+            pass
+        else:
+            self.power = self.charger.power
 
     def update_soc(self):
         """
         Update the soc of the vehicle when charging.
         """
-        if self.demand < 0:
-            raise ValueError('Vehicle was given negative amount of kWh.')
-        new_soc = self.soc + (self.demand / self.capacity) * 100
-        if new_soc > 100:
+        # target soc for charging the total kWh demand.
+        # target_soc = self.soc + (self.demand / self.capacity) * 100
+        # how many kWh can be charged per iteration with current power.
+        iter_demand = self.power * (self.time / 60)
+        new_soc = self.soc + (iter_demand / self.capacity) * 100
+        if new_soc >= 100:
             self.soc = 100
+            self.charger.remove_vehicle()
         else:
             self.soc = round(new_soc, 2)
-            self.charge = False
+            self.charging = False
 
     def find_charger(self):
         """
-        Uses charger if one is free, else waits until one becomes free.
+        Uses charger if one is free, else waits until next iteration to check again.
         """
         for charger in self.station.charge_list:
             if charger.available:
-                self.charge = True
-                charger.available = False
-                self.new_demand(self.new_route_len())
+                self.charger = charger
+                self.charging = True
+                self.power = charger.new_request()
+                self.new_demand()
                 self.update_soc()
                 break
 
@@ -115,7 +125,7 @@ class Vehicle(Agent):
         """
         Check which action to take for a vehicle.
         """
-        if self.charge:
+        if self.charging:
             self.update_soc()
         elif self.driving == 0:
             self.find_charger()
