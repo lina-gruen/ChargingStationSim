693d04577d87b6e47e0e8e7baf5985e93cb611de
lina-gruen <lina.grunbeck@gmail.com>
2023-02-01 15:00:06 +0100

    Add drive() and change driving variable from int to bool.

--- chargingStationSim/vehicle.py
+++ chargingStationSim/vehicle.py
@@ -13,8 +13,7 @@ class Vehicle(Agent):
     Base class for all vehicles in a vehicle fleet.
     """
 
-    default_params = {'weight_class': None, 'dist_type': None, 'capacity': None, 'efficiency': None,
-                      'drive_dist': None}
+    default_params = {'weight_class': None, 'dist_type': None, 'capacity': None, 'efficiency': None}
 
     # resolution = 600  # 10min
 
@@ -39,8 +38,6 @@ class Vehicle(Agent):
                     Max kWh rating of the vehicle battery.
                 efficiency: int
                     Efficiency of the battery measured in km/kWh.
-                drive_dist: int
-                    Length of the next trip planned in km.
         soc: int
             State of Charge of the vehicle battery.
         """
@@ -50,7 +47,6 @@ class Vehicle(Agent):
         self.weight_class = params['weight_class']
         self.dist_type = params['dist_type']
         self.capacity = params['capacity']
-        self.drive_dist = params['drive_dist']
         if params['efficiency'] is None:
             self.efficiency = 5
         else:
@@ -58,15 +54,15 @@ class Vehicle(Agent):
         self.soc = soc
         # kWh needed for the vehicle.
         self.demand = 0
-        # number of iterations the vehicle will be driving.
-        self.driving = 0
+        # if the vehicle is driving (True) or not (False).
+        self.driving = False
         '''
         # The power the vehicle is currently charging with.
         self.power = 0
         '''
         # The charger the vehicle is using. None if not charging.
         self.charger = None
-        # If the vehicle is charging or not.
+        # If the vehicle is charging (True) or not (False).
         self.charging = False
         # self.wait_time = None
         # self.max_pow = params['max_pow']
@@ -89,19 +85,30 @@ class Vehicle(Agent):
         Må finne ut hvor mye demand tilsvarer driving til neste kjøretur.
         '''
 
+    def drive(self):
+        """
+
+        """
+        speed = 60  # km/h
+        driving_demand = (self.efficiency * speed * self.time) / 60
+        new_soc = self.soc - (driving_demand / self.capacity) * 100
+        if new_soc <= 0:
+            self.soc = 0
+            self.driving = False
+        else:
+            self.soc = round(new_soc, 2)
+
     def update_soc(self):
         """
         Update the soc of the vehicle when charging.
         """
-
         '''
         target soc for charging the total kWh demand (enten 100 eller utifra demand, utifra
         om det er lokal eller langdistanse.)
         # target_soc = self.soc + (self.demand / self.capacity) * 100
         '''
-
         # how many kWh can be charged per iteration step with current power.
-        step_demand = self.charger.socket_power * (self.time / 60)
+        step_demand = self.charger.socket_power * (self.time / 60)  # min/60=h
         '''
         Sjå om ein faktisk trenge å oppdatere iter_demand hver runde. if self.power == self.charger.socket_power: pass
         '''
@@ -111,7 +118,7 @@ class Vehicle(Agent):
             self.charging = False
             self.charger.remove_vehicle()
             self.charger = None
-            # self.driving = ... ?
+            self.driving = True
         else:
             self.soc = round(new_soc, 2)
 
@@ -135,10 +142,10 @@ class Vehicle(Agent):
         """
         if self.charging:
             pass
-        elif self.driving == 0:
+        elif not self.driving:
             self.find_charger()
         else:
-            self.driving -= 1
+            self.drive()
 
     def step_1(self):
         """
@@ -150,7 +157,7 @@ class Vehicle(Agent):
         """
         Vehicle actions to execute for the second stage of each iteration of a simulation.
         """
-        print(f'Vehicle id: {self.id}, Charger id: {self.charger.id}, soc: {self.soc}')
+        # print(f'Vehicle id: {self.id}, Charger id: {self.charger.id}, soc: {self.soc}')
 
         for charger in self.station.charge_list:
             if charger.num_users != 0:
@@ -161,6 +168,9 @@ class Vehicle(Agent):
         if self.charging:
             print(f'Vehicle id: {self.id}, Charger id: {self.charger.id}, soc: {self.soc} '
                   f'\n----------------------------------------')
+        elif self.driving:
+            print(f'Vehicle id: {self.id}, Driving: True, soc: {self.soc} '
+                  f'\n----------------------------------------')
         else:
-            print(f'Vehicle id: {self.id}, Charger id: None, soc: {self.soc} '
+            print(f'Vehicle id: {self.id}, Waiting: True, soc: {self.soc} '
                   f'\n----------------------------------------')
