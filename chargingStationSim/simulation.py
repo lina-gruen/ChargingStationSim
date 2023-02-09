f132059684fcd5b2989b08dc5429e1a0e34f1446
lina-gruen <lina.grunbeck@gmail.com>
2023-02-09 10:58:05 +0100

    Divide time_step variable over 60 when passing to model_params dict and set sim_time to 24.
    This converts the time_step variable in the Station class to hours instead of minutes.

--- chargingStationSim/simulation.py
+++ chargingStationSim/simulation.py
@@ -11,14 +11,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Duration of simulation in hours.
-sim_time = 2
+sim_time = 24
 # Time resolution of each step in the simulation in minutes.
 time_step = 10
 # Number of steps the simulation requires.
 num_steps = int((sim_time/time_step)*60)
 
 # Set model parameters for a simulation.
-model_params = {'num_vehicle': 6, 'num_battery': 0, 'num_charger': 2, 'time_step': time_step}
+model_params = {'num_vehicle': 6, 'num_battery': 0, 'num_charger': 2, 'time_step': 60/time_step}
 
 # Start a simulation.
 results = batch_run(
@@ -36,15 +36,18 @@ results_df = pd.DataFrame(results)
 results_df.set_index(['Step', 'AgentID'], inplace=True)
 
 # Find final soc of all vehicles.
-end_soc = results_df.xs(num_steps, level="Step")["Soc"]
-
+end_soc = results_df.xs(num_steps, level='Step')['Soc']
 plt.figure()
 end_soc.hist(bins=range(int(results_df.Soc.max()) + 1))
 # plt.show()
 
-# Plot development of the soc of some vehicles.
+# Chosen arrival times for each vehicle.
+start_arrival = results_df.xs(0, level='Step')['Arrival']
+arrival = results_df.xs(1, level='AgentID')['Arrival']
+
+# Plot development of the soc for all vehicles.
 plt.figure()
-plt.plot(results_df.xs(3, level='AgentID')['Soc'], marker='o', label='Vehicle 3 soc')
-plt.plot(results_df.xs(5, level='AgentID')['Soc'], marker='o', label='Vehicle 5 soc')
-plt.legend()
-plt.show()
+for vehicle in range(model_params['num_vehicle']):
+    plt.plot(results_df.xs(vehicle, level='AgentID')['Soc'], marker='o', label=f'#{vehicle} soc')
+    plt.legend()
+    plt.show()
