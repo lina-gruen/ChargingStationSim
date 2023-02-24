10465f0a6ac55e5a00e5b20d22ea5cf09c092930
lina-gruen <lina.grunbeck@gmail.com>
2023-02-24 10:36:13 +0100

    Update battery test to changes make in the Station class and add fixture for a battery.

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -5,43 +5,42 @@ This file contains tests for the Battery class
 
 __author__ = 'Lina Grünbeck / lina.grunbeck@gmail.com'
 
+
+from chargingStationSim.station import Station
 from chargingStationSim.battery import Battery
 import pytest
-
-# from unittest import mock
 
 
 @pytest.fixture
 def battery():
-    battery = Battery(150, 100)
+    time_step = 10
+    station = Station(num_vehicle=0, num_battery=1, num_charger=0, time_step=60/time_step)
+    battery = Battery(unique_id=0, station=station, capacity=150, soc=90)
     return battery
 
 
-def test_get_soc():
+def test_get_soc(battery):
     """
     Tests if correct soc is returned.
     """
-    battery = Battery(150, 90)
     assert battery.get_soc() == 90
 
 
-def test_discharge():
+def test_discharge(battery):
     """
     Tests if battery discharges with the right
     amount and that soc is updated correctly.
     """
-    battery = Battery(100, 90)
     battery.discharge(10)
-    new_soc = 90 - (10 / 100) * 100
+    new_soc = round((90 - (10 / 150) * 100), 2)
     assert battery.soc == new_soc
 
 
-def test_recharge():
+def test_recharge(battery):
     """
     Tests if battery recharges with the right
     amount and that soc is updated correctly.
     """
-    battery = Battery(100, 90)
     battery.recharge(10)
-    new_soc = 90 + (10 / 100) * 100
+    new_soc = round((90 + (10 / 150) * 100), 2)
     assert battery.soc == new_soc
