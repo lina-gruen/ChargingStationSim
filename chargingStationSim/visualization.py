0dd7bf8b6710b86aeb9cd0f8e1192f155197d491
lina-gruen <lina.grunbeck@gmail.com>
2023-02-27 12:47:28 +0100

    Add duration plot and boxplot to station_plot().

--- chargingStationSim/visualization.py
+++ chargingStationSim/visualization.py
@@ -9,10 +9,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def station_plot(results, multirun=False, iterations=0, steps=0):
+def station_plot(results, multirun=False, iterations=0, resolution=60/10, sim_duration=24):
     data = pd.DataFrame(results)
     # Convert 10 min resolution to hours.
     data['Step'] *= (10/60)
+
+    # If only one run is made.
     if not multirun:
         data.set_index(['Step'], inplace=True)
 
@@ -23,19 +25,48 @@ def station_plot(results, multirun=False, iterations=0, steps=0):
         plt.ylabel('Power [kW]')
         plt.title('Station power')
         plt.show()
+    # If multiple runs are made, the mean of all runs is plotted.
     else:
         data.set_index(['iteration', 'Step'], inplace=True)
 
         # Development of the station power.
+        """
         plt.figure()
         for iter_num in range(iterations):
             plt.plot(data.xs(iter_num, level='iteration')['Power'])
+        """
+
+        # Dataframe with mean power for all runs.
+        mean_data = pd.DataFrame()
+        mean_data['load'] = data.groupby('Step')['Power'].mean()
 
         plt.figure()
-        plt.plot(data.groupby('Step')['Power'].mean())
+        plt.plot(mean_data)
         plt.xlabel('Time [h]')
         plt.ylabel('Power [kW]')
         plt.title('Station power')
+        plt.show()
+
+        # Boxplot for mean power over all runs.
+        """
+        plt.figure()
+        mean_data.plot.box()
+        plt.show()
+        """
+
+        # Duration plot for mean power over all runs.
+        mean_data['interval'] = resolution  # time per step in hours.
+        mean_data.sort_values(by=['load'], ascending=False, inplace=True)
+        mean_data['duration'] = mean_data['interval'].cumsum()
+        mean_data['percentage'] = mean_data['duration'] * 100 / sim_duration
+        mean_data.set_index('percentage', inplace=True)
+        reduced_mean = mean_data['load'].drop_duplicates()
+
+        plt.figure()
+        reduced_mean.plot()
+        plt.xlabel('Percentage [%]')
+        plt.ylabel('Load [kW]')
+        plt.title('Duration curve')
         plt.show()
 
     return data
