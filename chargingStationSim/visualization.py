c98bb77df44b7a226a47cdf59f68384d10928a37
lina-gruen <lina.grunbeck@gmail.com>
2023-02-27 21:39:49 +0100

    Add error plot.

--- chargingStationSim/visualization.py
+++ chargingStationSim/visualization.py
@@ -7,6 +7,9 @@ __author__ = 'Lina Grünbeck / lina.grunbeck@gmail.com'
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import sem
+sns.set_style('darkgrid')
 
 
 def station_plot(results, multirun=False, iterations=0, resolution=60/10, sim_duration=24):
@@ -36,15 +39,24 @@ def station_plot(results, multirun=False, iterations=0, resolution=60/10, sim_du
             plt.plot(data.xs(iter_num, level='iteration')['Power'])
         """
 
-        # Dataframe with mean power for all runs.
-        mean_data = pd.DataFrame()
-        mean_data['load'] = data.groupby('Step')['Power'].mean()
+        # Mean power and standard deviation for all runs.
+        mean_data = data.groupby('Step')['Power'].mean()
+        std_data = data.groupby('Step')['Power'].std()
 
         plt.figure()
         plt.plot(mean_data)
         plt.xlabel('Time [h]')
         plt.ylabel('Power [kW]')
         plt.title('Station power')
+        plt.show()
+
+        plt.figure()
+        line = mean_data
+        over_line = (mean_data - std_data)
+        under_line = (mean_data + std_data)
+        plt.plot(line, linewidth=2)
+        plt.fill_between(line.index, under_line,
+                         over_line, color='red', alpha=.3)
         plt.show()
 
         # Boxplot for mean power over all runs.
@@ -55,21 +67,23 @@ def station_plot(results, multirun=False, iterations=0, resolution=60/10, sim_du
         """
 
         # Duration plot for mean power over all runs.
-        mean_data['interval'] = resolution  # time per step in hours.
-        mean_data.sort_values(by=['load'], ascending=False, inplace=True)
-        mean_data['duration'] = mean_data['interval'].cumsum()
-        mean_data['percentage'] = mean_data['duration'] * 100 / sim_duration
-        mean_data.set_index('percentage', inplace=True)
-        reduced_mean = mean_data['load'].drop_duplicates()
+        dur_data = pd.DataFrame()
+        dur_data['mean'] = data.groupby('Step')['Power'].mean()
+        dur_data['interval'] = resolution  # time per step in hours.
+        dur_data.sort_values(by=['mean'], ascending=False, inplace=True)
+        dur_data['duration'] = dur_data['interval'].cumsum()
+        dur_data['percentage'] = dur_data['duration'] * 100 / sim_duration
+        dur_data.set_index('percentage', inplace=True)
+        reduced_dur = dur_data['mean'].drop_duplicates()
 
         plt.figure()
-        reduced_mean.plot()
+        reduced_dur.plot()
         plt.xlabel('Percentage [%]')
         plt.ylabel('Load [kW]')
         plt.title('Duration curve')
         plt.show()
 
-    return data
+    return mean_data
 
 
 def vehicle_plot(results):
