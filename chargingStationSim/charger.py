37952f2516d56a42a931371d5b2be4c06b07e365
lina-gruen <lina.grunbeck@gmail.com>
2023-03-04 11:12:03 +0100

    Remove update_power(), check num_vehicles in add_vehicle() and set accessible_power to max_power in init.

--- chargingStationSim/charger.py
+++ chargingStationSim/charger.py
@@ -24,23 +24,23 @@ class Charger:
         self.num_users = 0
         self.num_sockets = num_sockets
         self.max_power = power
-        self.socket_power = self.max_power
-        self.accessible_power = 0
+        # self.socket_power = self.max_power
+        self.accessible_power = self.max_power
 
-    def update_power(self):
-        """
-        Update the power delivered to each charging socket in use and the total power delivered by the whole charger.
-        """
-        if self.num_users != 0:
-            self.socket_power = self.max_power / self.num_users
-        else:
-            self.socket_power = self.max_power
+    # def update_power(self):
+    #     """
+    #     Update the power delivered to each charging socket in use and the total power delivered by the whole charger.
+    #     """
+    #     if self.num_users != 0:
+    #         self.socket_power = self.max_power / self.num_users
+    #     else:
+    #         self.socket_power = self.max_power
 
-        # used_power == max_power when power is distributed equally between sockets in use.
-        self.accessible_power = self.socket_power * self.num_users
-        """
-        Prøv å få til individuell effekt på hvert uttak dersom ulike effekter er ønskelig. Liste for socket_power?
-        """
+    #     # used_power == max_power when power is distributed equally between sockets in use.
+    #     self.accessible_power = self.socket_power * self.num_users
+    #     """
+    #     Prøv å få til individuell effekt på hvert uttak dersom ulike effekter er ønskelig. Liste for socket_power?
+    #     """
 
     # def check_new_power(self):
     #     """
@@ -55,14 +55,10 @@ class Charger:
     def add_vehicle(self, used_power):
         """
         Add a new vehicle to the charger.
-
-        Returns
-        -------
-        maximum power for each power outlet.
         """
         self.num_users += 1
         self.accessible_power -= used_power
-        if self.accessible_power == 0:
+        if self.accessible_power == 0 or self.num_users == self.num_sockets:
             self.available = False
 
         # if self.num_users == self.num_sockets:
