bf41e48f54dfcf4794235940e01eb4b1c61bee3b
lina-gruen <lina.grunbeck@gmail.com>
2023-03-06 16:24:12 +0100

    Add variables to model_params variable and add save_path variable as input to the functions from visualization.py.

--- chargingStationSim/simulation.py
+++ chargingStationSim/simulation.py
@@ -9,17 +9,21 @@ from chargingStationSim.station import Station
 from chargingStationSim.visualization import station_plot
 from chargingStationSim.visualization import vehicle_plot
 from mesa.batchrunner import batch_run
-
+from mesa.batchrunner import BatchRunner
 
 # Duration of simulation in hours.
 sim_time = 24
 # Time resolution of each step in the simulation in minutes.
 time_resolution = 10
 # Number of steps the simulation requires.
-num_steps = int((sim_time/time_resolution)*60) - 1
+num_steps = int((sim_time / time_resolution) * 60) - 1
+
+# Set number of vehicles for each vehicle type.
+# vehicles = (('Group_1', 5), ('Group_2', 20), ('Group_3', 15), ('Group_4', 10))
 
 # Set model parameters for a simulation.
-model_params = {'num_vehicle': 50, 'num_battery': 0, 'num_charger': 6,
+model_params = {'num_group1': 5, 'num_group2': 20, 'num_group3': 15, 'num_group4': 10,
+                'num_battery': 0, 'num_charger': 6,
                 'time_resolution': time_resolution, 'sim_time': sim_time}
 
 # Start a simulation.
@@ -33,6 +37,9 @@ results = batch_run(
     display_progress=True,
 )
 
+save_path = 'C:/Users/linag/OneDrive - Norwegian University of Life Sciences/Master/Plot'
+
 # Run functions for visualization of the simulation results.
-station_data = station_plot(results, multirun=True, iterations=10, time_step=time_resolution/60, sim_duration=sim_time)
+station_data = station_plot(results, multirun=True, iterations=10, time_step=time_resolution / 60,
+                            sim_duration=sim_time, path=save_path)
 # vehicle_data = vehicle_plot(results)
