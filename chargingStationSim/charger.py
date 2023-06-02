962243444b141badcd85140221d5cb39b52a8933
lina-gruen <76403153+lina-gruen@users.noreply.github.com>
2023-06-02 22:10:42 +0200

    Remove redundant comments.

--- chargingStationSim/charger.py
+++ chargingStationSim/charger.py
@@ -8,7 +8,7 @@ __author__ = 'Lina Grünbeck / lina.grunbeck@gmail.com'
 
 class Charger:
     """
-    Class for a charger.
+    Class for a charger connected to a charging station.
     """
 
     def __init__(self, power, num_sockets):
@@ -24,33 +24,7 @@ class Charger:
         self.num_users = 0
         self.num_sockets = num_sockets
         self.max_power = power
-        # self.socket_power = self.max_power
         self.accessible_power = self.max_power
-
-    # def update_power(self):
-    #     """
-    #     Update the power delivered to each charging socket in use and the total power delivered by the whole charger.
-    #     """
-    #     if self.num_users != 0:
-    #         self.socket_power = self.max_power / self.num_users
-    #     else:
-    #         self.socket_power = self.max_power
-
-    #     # used_power == max_power when power is distributed equally between sockets in use.
-    #     self.accessible_power = self.socket_power * self.num_users
-    #     """
-    #     Prøv å få til individuell effekt på hvert uttak dersom ulike effekter er ønskelig. Liste for socket_power?
-    #     """
-
-    # def check_new_power(self):
-    #     """
-    #     Checks what the socket power would be if the charger got an additional user.
-
-    #     Returns
-    #     -------
-    #     Socket power in kW.
-    #     """
-    #     return self.max_power / (self.num_users + 1)
 
     def add_vehicle(self, used_power):
         """
@@ -60,9 +34,6 @@ class Charger:
         self.accessible_power -= used_power
         if self.accessible_power == 0 or self.num_users == self.num_sockets:
             self.available = False
-
-        # if self.num_users == self.num_sockets:
-        #     self.available = False
 
     def remove_vehicle(self, freed_power):
         """
